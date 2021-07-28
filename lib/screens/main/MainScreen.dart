755fa51f23758a0173824480a644db428f7b1cc7
jlucaslopes <jlucasslhp@gmail.com>
2021-07-27 22:14:24 -0300

    Adcionado BottomAppBar

--- lib/screens/main/MainScreen.dart
+++ lib/screens/main/MainScreen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'screens/HistoricPage.dart';
+import 'screens/OrderPage.dart';
+import 'screens/ProfilePage.dart';
+
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
@@ -8,31 +12,148 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int currentTab = 0;
+  final List<Widget> screens = [
+    HistoricPage(),
+    OrderPage(),
+    ProfilePage(),
+  ];
+
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = HistoricPage();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: BottomAppBar(
-      shape: CircularNotchedRectangle(),
-      notchMargin: 10,
-      child: Container(
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                MaterialButton(
-                  minWidth: 40,
-                  onPressed: () {
-                    setState(() {});
-                  },
-                ),
-              ],
-            )
-          ],
+        body: PageStorage(
+          bucket: bucket,
+          child: currentScreen,
         ),
-      ),
-    ));
+        floatingActionButton: Container(
+          height: 70.0,
+          width: 70.0,
+          child: FloatingActionButton(
+            child: Icon(
+              Icons.cleaning_services,
+              size: 30,
+              color: Colors.white,
+            ),
+            backgroundColor: Color.fromRGBO(97, 46, 150, 1),
+            elevation: 20,
+            isExtended: true,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100),
+                side: BorderSide(color: Colors.black, width: 2)),
+            onPressed: () {
+              setState(() {
+                currentScreen = OrderPage();
+                currentTab = 2;
+              });
+            },
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          color: Color.fromRGBO(55, 10, 91, 30),
+          shape: CircularNotchedRectangle(),
+          notchMargin: 10,
+          child: Container(
+            color: Colors.transparent,
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    MaterialButton(
+                      minWidth: 40,
+                      onPressed: () {
+                        setState(() {
+                          currentScreen = HistoricPage();
+                          currentTab = 0;
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.history,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Meus Pedidos",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                //         Row(
+                //           crossAxisAlignment: CrossAxisAlignment.center,
+                //           children: [
+                //             MaterialButton(
+                //               minWidth: 40,
+                //               onPressed: () {
+                //                 setState(() {
+                //                   currentScreen = OrderPage();
+                //                   currentTab = 2;
+                //                 });
+                //               },
+                //               child: Column(
+                //                 mainAxisAlignment: MainAxisAlignment.center,
+                //                 children: [
+                //                   Icon(
+                //                     Icons.cleaning_services,
+                //                     color: currentTab == 2 ? Colors.blue : Colors.grey,
+                //                   ),
+                //                   Text(
+                //                     "Fáxina Ja",
+                //                     style: TextStyle(
+                //                       color:
+                //                           currentTab == 2 ? Colors.blue : Colors.grey,
+                //                     ),
+                //                   )
+                //                 ],
+                //               ),
+                //             ),
+                //           ],
+                //         ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    MaterialButton(
+                      minWidth: 40,
+                      onPressed: () {
+                        setState(() {
+                          currentScreen = ProfilePage();
+                          currentTab = 3;
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.person,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Perfil",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/screens/main/screens/HistoricPage.dart
+++ lib/screens/main/screens/HistoricPage.dart
@@ -11,8 +11,13 @@ class _HistoricPageState extends State<HistoricPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text('Historico'),
-    ));
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Color.fromRGBO(237, 205, 248, 100),
+        ),
+      ),
+    );
   }
 }
